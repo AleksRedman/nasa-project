d8e007ba41c05220f8bd4e204c697b0c52e10ab7
Aleksey <aleksey@datameshgroup.com>
2024-03-24 21:58:47 +0200

    Conect planets data to client and config deploying

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const cors =  require('cors');
+const path = require('path');
 
-const planetsRouter = require('./routes/planets/planets.router')
+const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
-app.use(express.json()) 
-app.use(planetsRouter) 
+app.use(cors({
+  origin: 'http://localhost:3001'
+}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(planetsRouter);
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
--- server/src/models/planets.module.js
+++ server/src/models/planets.module.js
@@ -1,3 +1,41 @@
-const planets = []
+const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse');
 
-module.exports = planets
+
+const hubitablePlanets = [];
+
+function isHubitable(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+};
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (data && isHubitable(data)) {
+          hubitablePlanets.push(data);
+        }
+    })
+    .on('error', (err) => {
+        console.log('---ERROR---', err)
+        reject(err)
+    })
+    .on('end', () => {
+        console.log('Num:', hubitablePlanets?.length || 0);
+        resolve()
+    });
+  });
+};
+
+module.exports = {
+  loadPlanetsData,
+  planets: hubitablePlanets
+};
